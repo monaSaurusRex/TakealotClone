a45fb2426f723d5b9da320901b4cf400a0a5c6cf
monaSaurusRex <monqiquenagel24.mn@gmail.com>
2023-04-21 12:45:29 +0200

    Created template for header layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductComponent } from './pages/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent}, //default route/path i path is nothing
+  {path: 'product/:productId', component: ProductComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'} //default page used if page doesn't exist
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductComponent } from './pages/product/product.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { SliderComponent } from './pages/slider/slider.component';
+import { ProductCardComponent } from './pages/product-card/product-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    HeaderComponent,
+    SliderComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
